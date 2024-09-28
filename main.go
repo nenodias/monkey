8e35c98539364dbacbd791c561a68c43c4611620
Nenodias <horacio.dias92@gmail.com>
2024-09-28 15:40:44 -0300

    Starting 2.1 Parsers

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
 func main() {
-	println("Hello World")
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type some Monkey code.")
+	repl.Start(os.Stdin, os.Stdout)
 }
